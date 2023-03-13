1d1fc47a2c223e3bfbc88d1581670806abe46d88
Fede-Coder <federiconieto04@gmail.com>
2023-03-13 20:14:07 -0300

    Fixed function createActivities

--- api/src/controllers/activities/createActivities.js
+++ api/src/controllers/activities/createActivities.js
@@ -9,7 +9,7 @@ async function createActivities(name, difficulty, duration, season, country) {
     if(!Array.isArray(country)) throw new Error('country must be an array')
     if(country.length < 1) throw new Error('mush contain at least 1 country')
 
-    if(await Activity.findOne()) throw new Error('')
+    if(await Activity.findOne({where: {name}})) throw new Error('already exists, type another activity name')
 
     const countries = await Country.findAll({where: {name: country}})
     const activity = await Activity.create({name, difficulty, duration, season})
